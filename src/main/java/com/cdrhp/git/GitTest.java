7dbe82be91d7eaf4e7a08f1fe47f508e7d775111
jiangjunfeng <2036217171@qq.com>
2022-02-01 22:27:47 +0800

    push test

--- src/main/java/com/cdrhp/git/GitTest.java
+++ src/main/java/com/cdrhp/git/GitTest.java
@@ -24,6 +24,7 @@ public class GitTest {
         System.out.println("hot fix test6");
         System.out.println("hot fix test7");
         System.out.println("hot fix test8");
+        System.out.println("push test");
     }
 
 }
