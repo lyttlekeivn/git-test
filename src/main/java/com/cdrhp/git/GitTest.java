6b59153d2b58a25a4cf91c1a8ac51ab30837ac33
jiangjunfeng <2036217171@qq.com>
2022-02-01 22:21:34 +0800

    'bbb'

--- src/main/java/com/cdrhp/git/GitTest.java
+++ src/main/java/com/cdrhp/git/GitTest.java
@@ -23,6 +23,7 @@ public class GitTest {
         System.out.println("hot fix test5");
         System.out.println("hot fix test6");
         System.out.println("hot fix test7");
+        System.out.println("hot fix test8");
     }
 
 }
